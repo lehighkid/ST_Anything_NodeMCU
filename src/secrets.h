b3b940f0f45430de0637f1152add46e0d9a9d9bc
lehighkid <lehighkid@gmail.com>
2018-03-21 19:03:38 -0400

    Added Secrets w Placeholders
    Added Secrets.h file with placeholders

---
+++ src/secrets.h
@@ -0,0 +1,9 @@
+#define WIFI_SSID "SSID"
+#define WIFI_PASSWORD "PASSWORD"
+#define DEVICE_IP_ADDRESS { 192, 0, 1, 211 } // Office Node 135; Basement Box 211
+#define ROUTER_GATEWAY { 192, 0, 1, 1 }
+#define LAN_SUBNET { 255, 255, 255, 0 }
+#define DNS_SERVER { 192, 0, 1, 21 }
+#define SERVER_PORT 8090
+#define HUB_IP_ADDRESS { 192, 0, 1, 137 } //139 for Hubitat; 137 for ST
+#define HUB_PORT 39500  //39501 for Hubitat; 39500 for ST
